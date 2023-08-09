c6124f52558ff77227e86d84c109885b36275f58
shobhakapgate <maushobha@gmail.com>
2023-08-09 08:59:48 +0530

    Done assignment of factorial number using foe loop
    Done assignment of factorial number using foe loop

---
+++ 06_factorialNumAssign.js
@@ -0,0 +1,15 @@
+
+function factorial(num){
+    let result = 1;
+for (let index = 1; index <= num ; index++) {
+    result = result * index;
+}   
+console.log(result);
+}
+
+factorial(5);
+factorial(9);
+factorial(11);
+factorial(7);
+
+  
